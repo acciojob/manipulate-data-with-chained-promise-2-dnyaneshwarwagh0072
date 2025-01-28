c068a826e7dc58d6b602d47957952119c591473f
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-01-28 19:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+function getNumbers() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+function updateOutput(text) {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = text;
+}
+
+getNumbers()
+    .then((numbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const evenNumbers = numbers.filter((num) => num % 2 === 0);
+                updateOutput(evenNumbers.join(", "));
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then((evenNumbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const multipliedNumbers = evenNumbers.map((num) => num * 2);
+                updateOutput(multipliedNumbers.join(", "));
+                resolve(multipliedNumbers);
+            }, 2000);
+        });
+    })
+    .catch((error) => {
+        console.error("An error occurred:", error);
+    });
